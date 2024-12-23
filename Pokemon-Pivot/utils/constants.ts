3853a8ffcdfe3fe58c4e64263443f57d36e3b936
Prince Yadav <139939808+callmeprinceyadav@users.noreply.github.com>
2024-12-24 00:34:46 +0530

    Delete Pokemon-Pivot directory

--- Pokemon-Pivot/utils/constants.ts
+++
@@ -1,4 +0,0 @@
-export const POKEMON_TYPES = [
-    'Normal', 'Fire', 'Water', 'Grass', 'Electric', 'Ice', 'Fighting', 'Poison', 'Ground', 'Flying', 'Psychic', 'Bug', 'Rock', 'Ghost', 'Dark', 'Dragon', 'Steel', 'Fairy'
-  ];
-  
